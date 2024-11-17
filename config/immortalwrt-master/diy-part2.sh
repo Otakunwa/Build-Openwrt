85244236e1d6b3bc66c982ce4e150dcaa46ded06
Otakunwa <135696507+Otakunwa@users.noreply.github.com>
2024-11-17 17:18:02 +0800

    Add luci-app-mosdns

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -33,7 +33,15 @@ echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "f
 # make package
 make package/luci-app-mihomo/compile
 
+# Add luci-app-mosdns
+# drop mosdns and v2ray-geodata packages that come with the source
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
 
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+make menuconfig # choose LUCI -> Applications -> luci-app-mosdns
+make package/mosdns/luci-app-mosdns/compile V=s
 
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
