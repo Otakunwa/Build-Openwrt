a05cd2e297c44fd010a20de4e526492d1dfe8b32
Otakunwa <135696507+Otakunwa@users.noreply.github.com>
2024-11-18 15:48:19 +0800

    Add Kenzok8's small-package source

--- config/openwrt-main/diy-part1.sh
+++ config/openwrt-main/diy-part1.sh
@@ -9,6 +9,11 @@
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
+# Add Kenzok8's small-package source
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+
 # other
 # rm -rf package/utils/{ucode,fbtest}
 
+# 对于强迫症的同学（有报错信息、或Lean源码编译出错的情况），请尝试删除冲突的插件
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
